327a46c476de1ef310b9bcc0014e716347c56a31
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-01-25 23:48:04 +0100

    Configuration files have been added

---
+++ src/core/config.py
@@ -0,0 +1,20 @@
+import os
+
+from pydantic import BaseSettings, Field
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+env_file = ".env"
+ENV_FILE_PATH = os.path.join(BASE_DIR, env_file)
+
+
+class Settings(BaseSettings):
+    app_debug_level: str = Field("INFO", env="APP_DEBUG_LEVEL")
+    base_dir: str = Field(BASE_DIR)
+    env_name: str = "Local"
+    base_url: str = "http://localhost:8000"
+    db_url: str = "sqlite:///./shortener.db"
+
+
+settings = Settings()
