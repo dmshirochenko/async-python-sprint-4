fb3667a255d2133f62dfe0cc87affdcb72fd4704
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-02-05 23:02:15 +0100

    url routes ping method improvments

--- src/api/v1/url_routes.py
+++ src/api/v1/url_routes.py
@@ -1,15 +1,23 @@
+import logging.config
 from typing import Optional, List
 
 import validators
 from fastapi import APIRouter, Depends, HTTPException, status, Header, Request, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import SQLAlchemyError, OperationalError, TimeoutError, DatabaseError
+
 
 from src.services.url_services import create_db_url, fetch_user_links, get_db_url_by_key, deactivate_db_url_by_key
 from src.db.db_connector import get_async_session
 from src.models.models import User
 from src.models.schemas import URLBase, LinkResponse, URLResponse
+from src.core.logger import LOGGING
+
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
+
 
 router = APIRouter()
 
@@ -37,8 +45,15 @@ async def ping_database(db: AsyncSession = Depends(get_async_session)):
     try:
         await db.execute(select(1))
         return {"message": "Database is reachable"}
-    except SQLAlchemyError:
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Database is not reachable")
+    except OperationalError:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Database connection error")
+    except TimeoutError:
+        raise HTTPException(status_code=status.HTTP_408_REQUEST_TIMEOUT, detail="Database connection timed out")
+    except DatabaseError:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="General database error")
+    except Exception as e:
+        logger.error("Unexpected error: %s", str(e))
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Unexpected error: {str(e)}")
 
 
 @router.post("/v1/url", response_model=URLResponse, status_code=status.HTTP_201_CREATED)
