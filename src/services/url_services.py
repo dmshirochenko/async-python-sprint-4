4e5f191173e27176d709eba6387fe4b13f6764f1
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-02-05 01:07:28 +0100

    links redirections has been added

--- src/services/url_services.py
+++ src/services/url_services.py
@@ -26,7 +26,7 @@ async def create_db_url(db: AsyncSession, url: URLBase, user_id: Optional[int] =
 
 
 async def fetch_user_links(db: AsyncSession, user_id: int) -> List[dict]:
-    result = await db.execute(select(URL).filter(URL.user_id == user_id))
+    result = await db.execute(select(URL).filter(URL.user_id == user_id, URL.is_active == True))
     links = result.scalars().all()
 
     response = [
@@ -36,15 +36,19 @@ async def fetch_user_links(db: AsyncSession, user_id: int) -> List[dict]:
     return response
 
 
-"""
-def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
-    return db.query(models.URL).filter(models.URL.key == url_key, models.URL.is_active).first()
+async def get_db_url_by_key(db: AsyncSession, key: str):
+    result = await db.execute(select(URL).filter(URL.key == key))
+    url = result.scalars().first()
+    return url
 
-def deactivate_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
-    db_url = get_db_url_by_secret_key(db, secret_key)
+
+async def deactivate_db_url_by_key(db: AsyncSession, key: str, user_id: int):
+    result = await db.execute(select(URL).filter(URL.key == key, URL.user_id == user_id))
+    db_url = result.scalars().first()
+
     if db_url:
         db_url.is_active = False
-        db.commit()
-        db.refresh(db_url)
-    return db_url
-"""
+        await db.commit()
+        return {"message": "URL has been successfully deactivated"}
+    else:
+        return None
